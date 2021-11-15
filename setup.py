e19073ad1de3146c571301e4faf694a6c1cbee27
swethasays <93997358+swethasays@users.noreply.github.com>
2021-11-15 09:14:09 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(name='ot65ygol',
+      version='0.1',
+      author='DSSS',
+      author_email='swetha.v.gatamaneni@fau.de',
+      packages=find_packages(),
+      install_requires=['numpy', 'Pillow', 'ipywidgets'])
